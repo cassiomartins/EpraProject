2b04feb8848d283aaa8b8a07ac4c178ab5d2de54
CassioMartins <cassio@infomais.com.br>
2021-10-06 23:47:02 -0300

    Documentando código

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -6,16 +6,6 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
 	public void StartRun()
 	{
 		SceneManager.LoadScene(1);
